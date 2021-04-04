f49e8c166c018ea8302b914855667de6b9fa8fc9
MathleteDev <mathletedev@gmail.com>
2021-04-04 11:11:24 -0700

    CRA with Material UI

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Button from "@material-ui/core/Button";
+import React from "react";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Button size="large" variant="contained" color="primary">
+					Hello World
+				</Button>
+			</header>
+		</div>
+	);
+}
+
+export default App;
